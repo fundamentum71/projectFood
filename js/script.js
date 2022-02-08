bfcad57521d9e151d54557cee8ffd293da93cfe8
Aleksandr <it.kucherov@gmail.com>
2022-02-08 10:23:59 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -56,12 +56,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			second: second,
 		};
 	}
-	//функция для приписывания нулей
-	function getZero(num) {
-		if (num >= 0 && num < 10) {
-			return `0${num}`;
-		} else return num;
-	}
 
 	function setClock(selector, endTime) {
 		const timer = document.querySelector(selector),
@@ -71,21 +65,73 @@ window.addEventListener('DOMContentLoaded', () => {
 			second = timer.querySelector('#seconds'),
 			timeInterval = setInterval(updateClock, 1000);
 
-		//вызвали функцию чтобы сразу обновлось значение
-		updateClock();
-
 		function updateClock() {
 			const t = getTimeRemaining(endTime);
+			days.innerHTML = t.days;
+			hours.innerHTML = t.hours;
+			minutes.innerHTML = t.minutes;
+			second.innerHTML = t.second;
 
-			days.innerHTML = getZero(t.days);
-			hours.innerHTML = getZero(t.hours);
-			minutes.innerHTML = getZero(t.minutes);
-			second.innerHTML = getZero(t.second);
-			//после того как отсчет дойдет до нуля, счет остановится.
 			if (t.total <= 0) {
 				clearInterval(timeInterval);
 			}
 		}
 	}
 	setClock('.timer', deadLine);
+
+	//modal window
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
+
+	function openModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		//завиксируем страницу чтобы нельзя было скролить при модальном окне
+		document.body.style.overflow = 'hidden';
+		//если пользователь сам уже открыл окно, мы очистим интервал
+		clearInterval(modalTimerId);
+	}
+
+	modalTrigger.forEach((btn) => {
+		btn.addEventListener('click', openModal);
+	});
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		//восстановим скрол
+		document.body.style.overflow = '';
+	}
+
+	modalCloseBtn.addEventListener('click', closeModal);
+	//закрытие кликом на подложку
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+	//закрытие на ESC
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	//вызов модалки через время
+	const modalTimerId = setTimeout(openModal, 5000);
+
+	function showModalByScroll() {
+		//Прокрученная часть+видимая часть которую мы прям сейчас видим на данный момент >=   полный сайт который у нас сейчас открыт (значит то что пользователь долистал до конца)
+		if (
+			window.pageYOffset + document.documentElement.clientHeight >=
+			document.documentElement.scrollHeight - 1
+		) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	//вызов модалки, когда пользователь долистает до конца страници
+	window.addEventListener('scroll', showModalByScroll);
 });
